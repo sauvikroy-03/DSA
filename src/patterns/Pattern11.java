da4841ca32c4fff701628905a4c42fc62986b494
sauvikroy-03 <sauvikroy3@gmail.com>
2026-07-27 11:46:05 +0530

    Pattern 11- Floyd's Triangle

---
+++ src/patterns/Pattern11.java
@@ -0,0 +1,18 @@
+package patterns;
+
+public class Pattern11 {
+    public static void main(String[] args) {
+
+        int z=1;
+        for(int i=1;i<6;i++){
+            for (int k=0;k<i;k++){
+                System.out.print(z);
+                System.out.print(" ");
+                z++;
+            }
+            System.out.println(" ");
+
+        }
+
+    }
+}
