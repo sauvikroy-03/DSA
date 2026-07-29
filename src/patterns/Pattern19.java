46de97e24ed6a320e4a5ebfc520d7d3b4a5f756a
sauvikroy-03 <sauvikroy3@gmail.com>
2026-07-29 22:58:31 +0530

    Pattern 19

---
+++ src/patterns/Pattern19.java
@@ -0,0 +1,37 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class Pattern19 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter the number of rows ");
+        int no_of_rows= sc.nextInt();
+        System.out.println("Enter the number of columns ");
+        int no_of_columns=sc.nextInt();
+
+        for(int i=0;i<no_of_rows;i++){
+            if(i>=1 && i!=no_of_rows-1){
+                for(int k=1;k>0;k--){
+                    System.out.print("*");
+                }
+                for(int k=0;k<=(no_of_columns-2)*2;k++){
+                    System.out.print(" ");
+
+                }
+                for(int k=1;k>0;k--){
+                    System.out.print("*");
+                }
+            }
+            else{
+                for(int k=no_of_columns;k>0;k--){
+                    System.out.print("*");
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+
+}
