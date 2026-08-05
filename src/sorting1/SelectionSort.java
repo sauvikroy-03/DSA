6b8babc69c36bfdb87ae64a69f30c3fef5e0d73d
sauvikroy-03 <sauvikroy3@gmail.com>
2026-08-05 23:16:22 +0530

    Sorting1: Selection Sort

---
+++ src/sorting1/SelectionSort.java
@@ -0,0 +1,28 @@
+package sorting1;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int[]arr={23,5,56,89,5,66,4,110};
+        selectionSort(arr);
+    }
+    public  static void selectionSort(int[] arr){
+
+
+
+        for(int i=0;i<arr.length;i++){
+            int min=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
