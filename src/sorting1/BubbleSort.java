af63f39549156e979cf6b61fa6c56c6fc9985e51
sauvikroy-03 <sauvikroy3@gmail.com>
2026-08-06 12:04:46 +0530

    Sorting1: Bubble Sort

--- src/sorting1/BubbleSort.java
+++ src/sorting1/BubbleSort.java
@@ -4,21 +4,26 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr={48,52,6,26,1};
+        int[] arr={1, 6, 26, 48, 52};
         bubblesort(arr);
     }
     public static void bubblesort(int[]arr){
         int key=arr.length;
+
         for(int i=0;i<arr.length;i++){
+            int didSwap=0;
             for(int j=0;j<key-1;j++){
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    didSwap=1;
                 }
-                System.out.println(Arrays.toString(arr));
             }
             key--;
+            if(didSwap==0){
+                break;
+            }
         }
         System.out.println(Arrays.toString(arr));
     }
