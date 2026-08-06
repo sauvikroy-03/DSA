9a255cee0fb72cac312808d811078218af094d09
sauvikroy-03 <sauvikroy3@gmail.com>
2026-08-06 21:29:53 +0530

    Sorting1: Insertion Sort

---
+++ src/sorting1/InsertionSort.java
@@ -0,0 +1,26 @@
+package sorting1;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {6, 1, 52, 48, 26};
+        insertionSort(arr);
+    }
+
+    public static void insertionSort(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
