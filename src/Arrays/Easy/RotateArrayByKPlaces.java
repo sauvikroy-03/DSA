f4e2e68e20bd74f8c05c4dc8f1434beae357ff80
sauvikroy-03 <sauvikroy3@gmail.com>
2026-08-11 23:01:09 +0530

    Arrays[Easy]: Rotate array by K places (left rotation)

--- src/Arrays/Easy/RotateArrayByKPlaces.java
+++ src/Arrays/Easy/RotateArrayByKPlaces.java
@@ -1,7 +1,37 @@
 package Arrays.Easy;
 
+import java.util.Arrays;
+
 public class RotateArrayByKPlaces {
     public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6,7};
+        int k=3;
+        int[] res=getResultantArray(arr,k);
+        System.out.println(Arrays.toString(res));
 
     }
+
+    public static  int[] getResultantArray(int[]arr,int k){
+        k=k%arr.length;
+reverse(arr,0,k-1);
+reverse(arr,k,arr.length-1);
+ return reverse(arr,0,arr.length-1);
+
+    }
+
+    public static int[] reverse(int[]arr,int low,int high){
+
+        while (low<high){
+            int temp=arr[low];
+            arr[low]=arr[high];
+            arr[high]=temp;
+            low++;
+            high--;
+
+        }
+        return arr;
+    }
+
+
+
 }
