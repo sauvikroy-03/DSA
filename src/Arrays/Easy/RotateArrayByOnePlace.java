7478f1625591728cbd9189c536b1d1ac9b20f77f
sauvikroy-03 <sauvikroy3@gmail.com>
2026-08-11 19:44:05 +0530

    Arrays[Easy]: Rotate Array by 1

---
+++ src/Arrays/Easy/RotateArrayByOnePlace.java
@@ -0,0 +1,28 @@
+package Arrays.Easy;
+
+import java.util.Arrays;
+
+public class RotateArrayByOnePlace {
+
+    public static void main(String[] args) {
+        int[]arr={1,2,7};
+        int k=1;
+        rotate(arr,k);
+    }
+    public static void rotate(int []arr,int k) {
+
+        if (arr.length > 0 ) {
+            k=k%arr.length;
+            for (int i = 0; i < k; i++) {
+                int temp = arr[0];
+                for (int j = 0; j < arr.length - 1; j++) {
+                    arr[j] = arr[j + 1];
+                }
+                arr[arr.length - 1] = temp;
+            }
+
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+}
+
