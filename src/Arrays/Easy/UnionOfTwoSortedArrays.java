30874ff156a3c929675895c8a3777d93a0ca8576
sauvikroy-03 <sauvikroy3@gmail.com>
2026-08-13 00:09:23 +0530

    Arrays[Easy]: Union of two sorted arrays..completed

--- src/Arrays/Easy/UnionOfTwoSortedArrays.java
+++ src/Arrays/Easy/UnionOfTwoSortedArrays.java
@@ -4,59 +4,46 @@ import sorting2.QuickSort;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+
 
 public class UnionOfTwoSortedArrays {
     public static void main(String[] args) {
-        int[]arr1={4,2,6,1};
-        int[]arr2={7,8,4,9,10};
+        int[]arr1={1,2,3,4};
+        int[]arr2={4,7,8,9,10};
         union(arr1,arr2);
 
     }
 
-    public static void quickSort(int[]arr,int low,int high){
 
-        if(low<high){
-            int p=partition(arr,low,high);
-            quickSort(arr,low,p-1);
-            quickSort(arr,p+1,high);
-        }
-
-    }
-    public static int partition(int[]arr,int low,int pivot){
-        int j=low;
-        int i=j-1;
-        while (j<=pivot){
-            if(arr[j]<arr[pivot]){
-                i++;
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
-            }
-            j++;
-        }
-        int temp=arr[i+1];
-        arr[i+1]=arr[pivot];
-        arr[pivot]=temp;
-
-        System.out.println(Arrays.toString(arr));
-
-
-return i+1;
-
-    }
 
     public static void union(int[]arr1,int[]arr2){
-        quickSort(arr1,0,arr1.length-1);
-        quickSort(arr2,0,arr2.length-1);
         int l=0;
         int r=0;
-        ArrayList<Integer> list=new  ArrayList<>();
-        for(int i=0;i<arr1.length;i++){
-
+        int k = 0; // Write pointer for arrX
+        int[]arrX=new int[arr1.length+arr2.length];
+        while (l < arr1.length && r < arr2.length) {
+            if (arr1[l] < arr2[r]) {
+                arrX[k++] = arr1[l++];
+            } else if (arr2[r] < arr1[l]) {
+                arrX[k++] = arr2[r++];
+            } else { // arr1[l] == arr2[r] (Duplicate)
+                arrX[k++] = arr1[l++];
+                r++; // Skip duplicate in arr2
+            }
         }
 
+        // 2. Copy remaining elements from arr1 (if any)
+        while (l < arr1.length) {
+            arrX[k++] = arr1[l++];
+        }
+
+        // 3. Copy remaining elements from arr2 (if any)
+        while (r < arr2.length) {
+            arrX[k++] = arr2[r++];
+        }
+
+        int[]res=Arrays.copyOf(arrX,k);
+System.out.println(Arrays.toString(res));
     }
 
 
