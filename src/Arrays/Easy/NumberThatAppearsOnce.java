b62d3ad683a298bd7188a2ce688bade14ecfb6a6
sauvikroy-03 <sauvikroy3@gmail.com>
2026-08-13 18:41:38 +0530

    Arrays[Easy]: Number that appear once

---
+++ src/Arrays/Easy/NumberThatAppearsOnce.java
@@ -0,0 +1,34 @@
+package numsays.Easy;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+
+public class NumberThatAppearsOnce {
+
+    public static void main(String[] args) {
+        int []nums={2,1,2,1,4,4};
+        findThatNumber(nums);
+    }
+
+    public static int findThatNumber(int[]nums){
+        HashMap<Integer,Integer>map=new  HashMap<>();
+        int res=-1;
+        for (int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+
+        for(Map.Entry<Integer,Integer>entry:map.entrySet()){
+            Integer value=entry.getValue();
+            Integer key=entry.getKey();
+            if(value==1){
+                res=key;
+                return key;
+            }
+        }
+
+        return res;
+
+    }
+}
