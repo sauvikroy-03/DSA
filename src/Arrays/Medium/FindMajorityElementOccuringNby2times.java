8f4efa8a97713295b04c8e4408cb1f081b8961c7
sauvikroy-03 <sauvikroy3@gmail.com>
2026-08-15 19:34:06 +0530

    Arrays[Medium]: Majority Element > N/2 times using Boyer-Moore Voting Algorithm

--- src/Arrays/Medium/FindMajorityElementOccuringNby2times.java
+++ src/Arrays/Medium/FindMajorityElementOccuringNby2times.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 public class FindMajorityElementOccuringNby2times {
 
     public static void main(String[] args) {
-        int[]arr={1, 2, 2};
-        findMajorityElement(arr);
+        int[]arr={1, 2, 2,1,5,1};
+        int res=findMajorityElement(arr);
+        System.out.println(res);
     }
-    public static  void findMajorityElement(int[] nums) {
+    public static  int findMajorityElement(int[] nums) {
 
-        int threshhold=nums.length/2;
         int count=0;
         int element =0;
         for(int i=0;i<nums.length;i++){
@@ -27,7 +27,20 @@ public class FindMajorityElementOccuringNby2times {
            }
         }
 
-        System.out.println(element);
+
+        int actualCount = 0;
+        for (int num : nums) {
+            if (num == element) {
+                actualCount++;
+            }
+        }
+
+        if (actualCount > nums.length / 2) {
+            return element; // Valid majority element found
+        }
+
+        return -1;
+
 
 
 
