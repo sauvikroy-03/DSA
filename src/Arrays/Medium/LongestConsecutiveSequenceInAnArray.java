1cf20640402527325aafb5b67f60dcb157952381
sauvikroy-03 <sauvikroy3@gmail.com>
2026-08-18 23:55:33 +0530

    Arrays[Medium]: Longest consecutive sequence in an array

--- src/Arrays/Medium/LongestConsecutiveSequenceInAnArray.java
+++ src/Arrays/Medium/LongestConsecutiveSequenceInAnArray.java
@@ -1,6 +1,7 @@
 package Arrays.Medium;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LongestConsecutiveSequenceInAnArray {
 
@@ -8,6 +9,7 @@ public class LongestConsecutiveSequenceInAnArray {
     int[]arr={100, 4, 200, 1, 3, 2};
     mergeSort(arr,0,arr.length-1);
     find(arr);
+
 
     }
     public static void mergeSort(int[]arr,int low,int high){
@@ -17,9 +19,43 @@ public class LongestConsecutiveSequenceInAnArray {
         int mid=(low+high)/2;
         mergeSort(arr,low,mid);
         mergeSort(arr,mid+1,high);
+        merge(arr,low,mid,high);
+    }
+    public static void merge(int[]arr,int low,int mid,int high){
+        int left=low;
+        int right= mid+1;
+        ArrayList<Integer> list=new ArrayList<>();
+
+
+        while (left<=mid&&right<=high){
+            if(arr[left]<=arr[right]){
+                list.add(arr[left]);
+                left++;
+            }
+            else if (arr[right]<=arr[left]){
+                list.add(arr[right]);
+                right++;
+            }
+        }
+        while (left<=mid){
+            list.add(arr[left]);
+            left++;
+        }
+        while (right<=high){
+            list.add(arr[right]);
+            right++;
+        }
+        for (int i = low; i <= high; i++) {
+            arr[i] = list.get(i - low);
+        }
+        System.out.println(Arrays.toString(arr));
     }
     public static void find(int[]arr){
-        ArrayList<Integer>list=new ArrayList<>();
+        int count=0;
+        int prev=0;
+        for (int i=0;i<arr.length;i++){
+
+        }
 
     }
 }
